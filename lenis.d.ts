35357f13b8eab57a292d3b5a3815be9ca52544ce
Shafayat Saad <dev.saad30@gmail.com>
2026-02-07 02:13:33 +0600

    Create lenis.d.ts

---
+++ lenis.d.ts
@@ -0,0 +1,18 @@
+declare module 'lenis' {
+  export default class Lenis {
+    constructor(options?: {
+      duration?: number;
+      easing?: (t: number) => number;
+      direction?: 'vertical' | 'horizontal';
+      gestureDirection?: 'vertical' | 'horizontal';
+      smooth?: boolean;
+      smoothTouch?: boolean;
+      touchMultiplier?: number;
+      infinite?: boolean;
+    });
+    raf(time: number): void;
+    destroy(): void;
+    on(event: string, callback: (args: any) => void): void;
+    scrollTo(target: any, options?: any): void;
+  }
+}
