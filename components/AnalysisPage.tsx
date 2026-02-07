bd23d3c2e764365cece9195ba4cbd1c2d07826e0
Shafayat Saad <dev.saad30@gmail.com>
2026-02-08 03:26:46 +0600

    Update AnalysisPage.tsx

--- components/AnalysisPage.tsx
+++ components/AnalysisPage.tsx
@@ -624,7 +624,6 @@ export const AnalysisPage: React.FC<AnalysisPageProps> = ({ onNavigate }) => {
                                     desc={v.desc}
                                     riskScore={v.riskScore}
                                     action={v.action}
-                                    onClick={() => setSelectedVuln(v)}
                                 />
                             ))
                         ) : (
